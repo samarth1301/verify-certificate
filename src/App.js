cf4a092380b9564919c56449634bf4f9c7992a67
samarth1301 <samarthsingh890.ss@gmail.com>
2022-06-19 18:02:32 +0530

    routing and tailwind added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Routes>
+      <Route path='/' element={<h1>verify here</h1>}/>
+      <Route path='/:id' element={<h1>certi here</h1>}/>
+    </Routes>
+  </Router>
   );
 }
 
